bc1e81f903db45aa7ee65be90e5cd7e6682c3d26
Surovtsev Alexander <alexander.surovtsev.ya@yandex.ru>
2026-05-21 07:14:07 +0300

    update

--- kafka-consumer/main.go
+++ kafka-consumer/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/swaggo/http-swagger"
+	"consumer/docs"
 	"log"
 	"net/http"
 )
@@ -8,10 +10,27 @@ import (
 func main() {
 	log.Println("Consumer stated")
 
-	http.HandleFunc("/send", addMessageHandler)
+	docs.SwaggerInfo.Title = "Kafka Consumer API"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Description = "API for sending messages to Kafka"
+	docs.SwaggerInfo.BasePath = "/"
+
+	http.HandleFunc("/receive", addMessageHandler)
+
+	http.HandleFunc("/swagger/", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8086/swagger/doc.json"),
+	))
+
 	log.Fatal(http.ListenAndServe(":8086", nil))
 }
 
+// @Summary Receive message
+// @Description Receive message from Kafka
+// @Tags messages
+// @Accept json
+// @Produce json
+// @Success 200 {object} map[string]string
+// @Router /receive [get]
 func addMessageHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status": "message received (stub)"}`))
